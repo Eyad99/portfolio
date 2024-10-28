2af6640a2ee438514216cb9ddd41540f21a5a141
eyad sharaf almasri <iead.sh123@gmail.com>
2024-10-28 15:45:22 +0300

    change scroll design

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <main className="snap-y snap-mandatory overflow-y-scroll h-screen">
+        <main className="snap-y snap-mandatory overflow-y-scroll h-screen scrollbar">
           <Header />
           {children}
           <RouadMap />
