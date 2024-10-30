0458a3a1d8876f3aa5a50f32f143aa572791e8a0
eyad sharaf almasri <iead.sh123@gmail.com>
2024-10-31 01:51:20 +0300

    add image to project

--- components/companies/cubeta/rush/index.tsx
+++ components/companies/cubeta/rush/index.tsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import Paginate from '@/components/reusable-components/paginate';
 import Link from 'next/link';
-import { Carousel, CarouselContent, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
-
-// import CustomProductImage from "@/assets/projects/street-league/custom-product.png";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+import Image from 'next/image';
+import DashboardImage from '@/assets/projects/rush/dashboard.png';
+import MapImage from '@/assets/projects/rush/map.png';
+import SignInImage from '@/assets/projects/rush/sign-in.png';
+import DriverImage from '@/assets/projects/rush/driver.png';
+import OrderImage from '@/assets/projects/rush/order.png';
+import OrderDetailsImage from '@/assets/projects/rush/order-details.png';
+import PaymentImage from '@/assets/projects/rush/payment.png';
 
 const Rush = () => {
+	const ImagesEmun = [
+		{ image: SignInImage, alt: 'SignInImage' },
+		{ image: DashboardImage, alt: 'DashboardImage' },
+		{ image: MapImage, alt: 'MapImage' },
+		{ image: DriverImage, alt: 'DriverImage' },
+		{ image: OrderImage, alt: 'OrderImage' },
+		{ image: OrderDetailsImage, alt: 'OrderDetailsImage' },
+		{ image: PaymentImage, alt: 'PaymentImage' },
+	];
 	return (
 		<div className='flex flex-col justify-center items-center m-28 gap-16'>
-			<h1 className='font-bold text-4xl '>Dtic</h1>
+			<h1 className='font-bold text-4xl '>Rush</h1>
 
 			<Carousel
 				opts={{
 					align: 'start',
 				}}
-				className='w-full max-w-8xl'
+				className='w-full  max-w-8xl'
 			>
-				<CarouselContent>
-					{/* {ImagesEmun.map((image, index) => (
-            <CarouselItem key={index}>
-              <div className="p-1 ">
-                <Image objectFit="fill" src={image.image} alt={image.alt} />
-              </div>
-            </CarouselItem>
-          ))} */}
+				<CarouselContent className=''>
+					{ImagesEmun.map((image, index) => (
+						<CarouselItem key={index}>
+							<div className='p-1 '>
+								<Image objectFit='fill' src={image.image} alt={image.alt} className='shadow-md' />
+							</div>
+						</CarouselItem>
+					))}
 				</CarouselContent>
 				<CarouselPrevious />
 				<CarouselNext />
