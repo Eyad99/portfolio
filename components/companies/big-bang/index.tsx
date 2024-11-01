c078b8573bdcc5ad31af8d601a503c3eda9245b4
eyad sharaf almasri <iead.sh123@gmail.com>
2024-11-01 18:50:48 +0300

    just push

--- components/companies/big-bang/index.tsx
+++ components/companies/big-bang/index.tsx
@@ -1,40 +1,37 @@
-import React from "react";
-import ProjectCard from "../../reusable-components/project-card";
-import { Project } from "@/core";
-import dticLogo from "@/assets/img/dtic.png";
-import Paginate from "@/components/reusable-components/paginate";
+import React from 'react';
+import ProjectCard from '../../reusable-components/project-card';
+import { Project } from '@/core';
+import dticLogo from '@/assets/img/dtic.png';
+import Paginate from '@/components/reusable-components/paginate';
 
 const BigBang = () => {
-  const projects: Project[] = [
-    {
-      logo: dticLogo.src,
-      title: "Dtic",
-      link: "dtic",
-      url: "https://dtic.co/",
-    },
-  ];
-  return (
-    <section
-      className={`flex flex-col justify-center m-auto sm:w-[75%] w-full p-6 gap-10`}
-      aria-hidden={true}
-    >
-      <div className="flex flex-col justify-center items-center gap-4">
-        <h1 className="font-bold">
-          <span className="text-4xl">/</span>
-          <span className="text-[#474747] text-3xl">Work.</span>
-        </h1>
-        <p className="text-xl font-bold text-center ">
-          A collection of projects I've worked on.
-        </p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-        {projects.map((project, index) => (
-          <ProjectCard key={index} project={project} />
-        ))}
-      </div>
-      <Paginate text="Next Company" link="/company/go-dev" />
-    </section>
-  );
+	const projects: Project[] = [
+		{
+			logo: dticLogo.src,
+			title: 'Dtic',
+			link: 'dtic',
+			url: 'https://dtic.co/',
+		},
+	];
+	console.log('TEST');
+
+	return (
+		<section className={`flex flex-col justify-center m-auto sm:w-[75%] w-full p-6 gap-10`} aria-hidden={true}>
+			<div className='flex flex-col justify-center items-center gap-4'>
+				<h1 className='font-bold'>
+					<span className='text-4xl'>/</span>
+					<span className='text-[#474747] text-3xl'>Work.</span>
+				</h1>
+				<p className='text-xl font-bold text-center '>A collection of projects I've worked on.</p>
+			</div>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full'>
+				{projects.map((project, index) => (
+					<ProjectCard key={index} project={project} />
+				))}
+			</div>
+			<Paginate text='Next Company' link='/company/go-dev' />
+		</section>
+	);
 };
 
 export default BigBang;
